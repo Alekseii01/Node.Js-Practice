d2369d1d81428403b4b9dbc1469e97efeece3872
Aleksei <zidvi33@gmail.com>
2025-10-30 19:22:02 +0200

    feat: add ArticleEdit route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import ArticleList from './components/ArticleList/ArticleList';
 import ArticleView from './components/ArticleView/ArticleView';
 import ArticleCreate from './components/ArticleCreate/ArticleCreate';
+import ArticleEdit from './components/ArticleEdit/ArticleEdit';
 import './App.css';
 
 console.log('API URL:', import.meta.env.VITE_API_URL);
@@ -27,6 +28,7 @@ function App() {
               <Route path="/" element={<ArticleList />} />
               <Route path="/article/:id" element={<ArticleView />} />
               <Route path="/create" element={<ArticleCreate />} />
+              <Route path="/edit/:id" element={<ArticleEdit />} />
               <Route path="*" element={<h2>404: Page Not Found</h2>} />
             </Routes>
           </div>
