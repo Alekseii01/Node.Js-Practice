a752cddae14efbc207d7b2d6cde308219e0ea213
Aleksei <zidvi33@gmail.com>
2025-12-07 18:29:47 +0200

    feat: add Workspace CRUD

--- client/src/components/WorkspaceSelector/WorkspaceSelector.jsx
+++ client/src/components/WorkspaceSelector/WorkspaceSelector.jsx
@@ -1,14 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useWebSocket } from '../../context/WebSocketContext';
 import './WorkspaceSelector.css';
 
 function WorkspaceSelector({ selectedWorkspace, onWorkspaceChange }) {
   const [workspaces, setWorkspaces] = useState([]);
   const [loading, setLoading] = useState(true);
+  const { lastMessage } = useWebSocket();
 
   useEffect(() => {
     fetchWorkspaces();
   }, []);
+
+  useEffect(() => {
+    if (lastMessage) {
+      const { type } = lastMessage;
+      if (type === 'workspace_added' || type === 'workspace_updated' || type === 'workspace_deleted') {
+        fetchWorkspaces();
+      }
+    }
+  }, [lastMessage]);
 
   const fetchWorkspaces = async () => {
     try {
